0f9ab13ca95d0b18cbd488131dc4fedfe5751a55
djjkc <102584654+djjkc@users.noreply.github.com>
2022-07-01 09:38:36 +1000

    Delete completed

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,11 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 class Pledge(models.Model):
     amount = models.IntegerField()
     comment = models.CharField(max_length=200)
     anonymous = models.BooleanField()
-    project = models.ForeignKey('Project',on_delete=models.CASCADE,related_name='pledges')
-    supporter = models.CharField(max_length=200)
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='supporter_pledges'
+    )
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
@@ -14,5 +23,9 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
-    owner = models.CharField(max_length=200)
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+    )
 
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -6,9 +6,10 @@ from rest_framework.response import Response
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer, PledgeDetailSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
+from .permissions import IsOwnerOrReadOnly
 
-
+#/pledges/
 class PledgeList(APIView):
     def get(self, request):
         pledges = Pledge.objects.all()
@@ -22,7 +23,9 @@ class PledgeList(APIView):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+# /projects/
 class ProjectList(APIView):
+
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -31,14 +34,19 @@ class ProjectList(APIView):
     def post(self, request):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+# /projects/<int:p>/
 class ProjectDetail(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            project =Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request,project)
+            return project
         except Project.DoesNotExist:raise Http404
         
     def get(self, request, pk):
@@ -49,11 +57,20 @@ class ProjectDetail(APIView):
     def delete(self, request, pk):
         project = self.get_object(pk)
         if project:
-            ProjectList.delete()
+            project.delete()
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,data=data,partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
 
-
+# /pledges/<int:p>/
 class PledgeDetail(APIView):
     def get_object(self, pk):
         try:
