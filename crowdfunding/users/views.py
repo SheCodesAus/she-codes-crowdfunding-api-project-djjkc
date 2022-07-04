84811e3fc79492a9a19b03b3ea8e8e1736094461
djjkc <102584654+djjkc@users.noreply.github.com>
2022-07-04 13:46:50 +1000

    Updated User Views status

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -18,8 +18,8 @@ class CustomUserList(APIView):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
 class CustomUserDetail(APIView):
     def get_object(self, pk):
